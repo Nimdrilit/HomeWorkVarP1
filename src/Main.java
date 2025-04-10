bd3eaf49988ac7025e9694a1339622dd8da3a918
Nimdrilit <nimdrilir@gmail.com>
2025-04-10 20:36:46 +0500

    task 8

--- src/Main.java
+++ src/Main.java
@@ -57,5 +57,4 @@ public class Main {
     var newWorkingTime;
     var newWorkingTime = moreEmploers * 8;
     System.out.println("Если в компании работает " + newWorkingTime + "человек, то всего " + newWorkingTime + "часов работы может быть поделено между сотрудниками.");
-
 }
